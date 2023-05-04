5ab123434d4193f1413530d7e7dd079ba591383e
Asher Ding <ashserding@icloud.com>
2023-05-04 21:52:54 +0800

    add some pages

---
+++ src/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<Navbar></Navbar>
+		<Chatpage></Chatpage>
+	</view>
+</template>
+
+<script>
+import Navbar from 'components/navbar.vue'
+import Chatpage from 'components/chatpage.vue'
+export default {
+	name: 'Index',
+	components: {
+		Navbar,
+		Chatpage
+	},
+	data() {
+		return {
+			messages: [
+				{ text: "Hi there!", isMine: false },
+				{ text: "Hey! What's up?", isMine: true }
+			],
+			newMessageText: ''
+		};
+	},
+	onLoad() {
+
+	},
+	methods: {
+		toggleMenu() {
+			console.log('toggleMenu')
+		},
+		sendMessage() {
+			this.messages.push({
+				text: this.newMessageText,
+				isMine: true
+			})
+			this.newMessageText = ""
+		}
+	}
+}
+</script>
+
+<style></style>
