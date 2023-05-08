ba47661311ba29c4a2a4008a1c4ec89ffbba41f6
Asher Ding <ashserding@icloud.com>
2023-05-08 21:46:51 +0800

    完成左侧抽屉的初步构想

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="index">
     <!-- 导航栏 -->
-    <uni-nav-bar :fixed="true" shadow leftText="列表" leftIcon="chatboxes-filled" statusBar="true" @click="showDrawer">
+    <uni-nav-bar :fixed="true" shadow leftText="列表" leftIcon="chatboxes-filled" statusBar="true" @clickLeft="showDrawer">
     </uni-nav-bar>
+
     <!-- 抽屉 -->
-    <uni-drawer ref="showRight" mode="right" :mask-click="false">
-      <scroll-view style="height: 100%;" scroll-y="true">
-        <button @click="closeDrawer">关闭Drawer</button>
-        <view v-for="item in 60" :key="item">可滚动内容 {{ item }}</view>
+    <uni-drawer ref="showLeft" mode="left" :width="280" :mask-click="ture">
+      <scroll-view style="height: 60%;" scroll-y="true">
+        <!-- 占位符 -->
+        <view style="margin-top: 16%"></view>
+        <!-- 个人信息 -->
+        <view style="display: flex; align-items: center; padding: 8px; border-bottom: 1px solid gray;">
+          <uni-icons type="vip-filled" size="30"></uni-icons>
+          <text style="margin-left: 10px; font-size: 18px;">搭子</text>
+        </view>
+        <uni-list>
+          <uni-list :border="true">
+            <!-- 显示圆形头像 -->
+            <uni-list-chat :avatar-circle="true" title="uni-app"
+              avatar="https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png" note="您收到一条新的消息"
+              time="2020-02-02 20:20"></uni-list-chat>
+            <!-- 右侧带角标 -->
+            <uni-list-chat title="uni-app" avatar="https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+              note="您收到一条新的消息" time="2020-02-02 20:20" badge-text="12"></uni-list-chat>
+            <!-- 头像显示圆点 -->
+            <uni-list-chat title="uni-app" avatar="https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+              note="您收到一条新的消息" time="2020-02-02 20:20" badge-positon="left" badge-text="dot"></uni-list-chat>
+            <!-- 头像显示角标 -->
+            <uni-list-chat title="uni-app" avatar="https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+              note="您收到一条新的消息" time="2020-02-02 20:20" badge-positon="left" badge-text="99"></uni-list-chat>
+            <!-- 显示多头像 -->
+            <uni-list-chat title="uni-app" :avatar-list="avatarList" note="您收到一条新的消息" time="2020-02-02 20:20"
+              badge-positon="left" badge-text="dot"></uni-list-chat>
+            <!-- 自定义右侧内容 -->
+            <uni-list-chat title="uni-app" :avatar-list="avatarList" note="您收到一条新的消息" time="2020-02-02 20:20"
+              badge-positon="left" badge-text="dot">
+              <view class="chat-custom-right">
+                <text class="chat-custom-text">刚刚</text>
+                <!-- 需要使用 uni-icons 请自行引入 -->
+                <uni-icons type="star-filled" color="#999" size="18"></uni-icons>
+              </view>
+            </uni-list-chat>
+          </uni-list>
+        </uni-list>
+        <!-- <view v-for="item in 1" :key="item">可滚动内容 {{ item }}</view> -->
       </scroll-view>
     </uni-drawer>
     <!-- <Navbar></Navbar> -->
@@ -34,15 +70,30 @@
 // import Chatpage from '@/components/chatpage.vue';
 import Bulletin from '@/components/card/bulletin.vue';
 import Entry from '@/components/entry.vue'
+import { getCurrentInstance } from 'vue';
+
+const ctx = getCurrentInstance().ctx
+// 这里的ctx等于vue2里面的 this
+
+
 
 // TODO: 需要解决导航栏的按键绑定问题
-function showDrawer() {
-  // .$refs.showRight.open();
-  console.log('showDrawer')
+function showDrawer(e) {
+  ctx.$refs.showLeft.open()
+  // console.log(instance.ctx.$refs.showRight)
+  // console.log('showDrawer')
 }
 function closeDrawer() {
   console.log('closeDrawer')
 }
+
+const avatarList = [{
+  url: 'https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png'
+}, {
+  url: 'https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png'
+}, {
+  url: 'https://web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png'
+}]
 
 
 // onUnload(() => {
