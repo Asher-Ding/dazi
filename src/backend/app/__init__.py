a8131ed84fdc249c3c1fedbc626a5dcb32e0a7de
Asher Ding <ashserding@icloud.com>
2023-05-09 13:46:37 +0800

    初始化backend

---
+++ src/backend/app/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2023/05/09 12:35:05
+@Author  :   Asher Ding 
+@Version :   0.1.0
+@Contact :   asherding@icloud.com
+@License :   (C)Copyright 2023-2023, Asher Ding
+@Desc    :   None
+'''
+__version__ = '0.1.0'
---
+++ src/backend/app/configs/default.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   default.py
+@Time    :   2023/05/09 12:42:31
+@Author  :   Asher Ding 
+@Version :   0.1.0
+@Contact :   asherding@icloud.com
+@License :   (C)Copyright 2023-2023, Asher Ding
+@Desc    :   None
+'''
+import os
+import sys
+
+class DefaultConfig(object):
+    # Get the app root path
+    # 获取当前文件所在目录的上三级目录即backend目录
+    basedir = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(
+                           os.path.dirname(__file__)))))
+    
+    # Python version
+    py_version = '{0.major}.{0.minor}.{0.micro}'.format(sys.version_info)
+
+    # Flask Settings
+    DEBUG = True
+    TESTING = False
