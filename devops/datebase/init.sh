696eb99ebe1a5c792f8018dc59f9956d691751cf
Asher Ding <ashserding@icloud.com>
2023-05-12 20:47:24 +0800

    成功访问vault；成功访问mongo、mongoexpress

--- devops/datebase/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Replace these variables with your own
-MONGO_HOST="localhost"
-MONGO_PORT="27017"
-
-# Run Mongo command to create new database
-mongo --host $MONGO_HOST --port $MONGO_PORT --eval 'db = db.getSiblingDB("dazi"); db.createCollection("sample");'
